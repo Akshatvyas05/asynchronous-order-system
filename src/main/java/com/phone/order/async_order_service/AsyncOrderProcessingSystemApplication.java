c6ce370faef7cb00246793a5d2bd590c070f1468
Akshatvyas05 <akshatvyas0@gmail.com>
2025-05-25 18:04:43 +0530

    feat: initial commit

---
+++ src/main/java/com/phone/order/async_order_service/AsyncOrderProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.phone.order.async_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncOrderProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncOrderProcessingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phone/order/async_order_service/AsyncOrderProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phone.order.async_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncOrderProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
